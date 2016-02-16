559dd6f7e79f38e0d928ff0103c8987b4d27ccd2
Polcode <Polcode>
2016-02-16 18:22:55 +0100

    List of projects

--- wp-content/plugins/wpdoc/wpdoc.php
+++ wp-content/plugins/wpdoc/wpdoc.php
@@ -71,91 +71,31 @@ function wpdoc_uninstall() {
 */
 function wpdoc_plugin_menu() {
 	add_menu_page('WPdoc', 'WPdoc', 'administrator', 'wpdoc_settings', 'wpdoc_display_settings');
-	add_submenu_page('wpdoc_projects', __('Images'), __('Images'), 'edit_themes', 'wpdoc_projects', 'wpdoc_projects');
+	//add_submenu_page('wpdoc_projects', __('Images'), __('Images'), 'edit_themes', 'wpdoc_projects', 'wpdoc_projects');
 }
 
+function wpdoc_settings() {
+    echo 123;
+}
 
-
-
-
-
+function wpdoc_display_settings() {
+    echo '<div class="wrap"><form action="options.php" method="post" name="options">';
+    echo '<h2>Select Your Settings</h2>';
+    //echo wp_nonce_field('update-options');
+    echo '<table class="form-table" width="100%" cellpadding="10">';
+    echo '  <tr>';
+    echo '      <th>Projekt</th>';
+    echo '  </tr>';
+    echo '</table>';
+    
+    echo '</table>';
+    echo '<input type="hidden" name="action" value="update'.__('Update').'" />';
+    echo ''; 
+    echo '<input type="hidden" name="page_options" value="rmlc_speed,rmlc_type,rmlc_interval" />';
+    echo ''; 
+    echo '<input type="submit" name="Submit" value="Update" /></form></div>';
+}
 
 add_action('admin_menu', 'wpdoc_plugin_menu');
 register_deactivation_hook(__FILE__, 'wpdoc_uninstall');
 register_activation_hook(__FILE__, 'wpdoc_install');
-
-
-
-
-
-
-
-
-/**
- * Wyswietla formularz do edycji obrazkow
- */
-function wpdoc_images() {
-	$model=new LogoCarousel();
-	if (isset($_POST['rmlc_images'])) {
-		$model->deleteAll();
-		foreach ($_POST['rmlc_images'] as $img) {
-			$model->add(array('image' => $img['image'], 'link' => $img['link'], 'title' => $img['title']));
-		}
-	}
-	$results = $model->getAll();
-
-	echo '<h2>' . __('Images') . '</h2>';
-	echo '<form action="?page=rmlc_images" method="post">';
-	echo '<table class="form-table" style="width:auto;" cellpadding="10">
-        <thead>
-        <tr>
-        <td>' . __('Image') . '</td><td>' . __('Link') . '</td><td>' . __('Title') . '</td><td>' . __('Delete') . '</td>
-        </tr>
-        </thead>
-        <tbody class="items">';
-	$i=0;
-	foreach ($results as $row) {
-		echo '<tr>
-            <td><input name="rmlc_images['.$i.'][image]" type="text" value="' . $row['image'] . '" /></td>';
-		echo '<td><input name="rmlc_images['.$i.'][link]" type="text" value="' . $row['link'] . '" /></td>';
-		echo '<td><input name="rmlc_images['.$i.'][title]" type="text" value="' . $row['title'] . '" /></td>';
-		echo '<td><a class="delete" href="">' . __('Delete') . '</a></td>
-            </tr>';
-		$i++;
-	}
-	echo '</tbody><tr><td colspan="4"><a class="add" href="">' . __('Add') . '</a></td></tr>';
-	echo '<tr><td colspan="4"><input type="submit" value="' . __('Save') . '" /></td></tr>';
-	echo '</table>';
-	echo '</form>';
-	 
-	echo '
-        <script type="text/javascript">
-        jQuery(document).ready(function($) {
-        $("table .delete").click(function() {
-        $(this).parent().parent().remove();
-        return false;
-        });
-        $("table .add").click(function() {
-        var count = $("tbody.items tr").length+1;
-        var code=\'<tr><td><input type="text" name="rmlc_images[\'+count+\'][image]" /></td><td><input type="text" name="rmlc_images[\'+count+\'][link]" /></td><td><input type="text" name="rmlc_images[\'+count+\'][title]" /></td><td><a class="delete" href="">' . __('Delete') . '</a></td></tr>\';
-        $("tbody.items").append(code);
-        return false;
-        });
-        });
-        </script>
-        ';
-}
-
-
-
-
-
-
-
-
-
-/*
-echo plugin_dir_path() . 'wpdoc';
-echo '<br>';
-echo plugins_url() . 'wpdoc';
-*/
